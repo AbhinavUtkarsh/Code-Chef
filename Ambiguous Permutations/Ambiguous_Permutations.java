dea76e9f8863056db9d1fdb26eaa4cd900537adf
Abhinav Utkarsh <abhinav.utkarsh@gmail.com>
2019-09-27 11:27:24 +0530

    Ambiguous Permutations added and Small Factorial Solved

---
+++ Ambiguous Permutations/Ambiguous_Permutations.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Ambiguous_Permutations {
+
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String args[]) {
+        Ambiguous_Permutations AP = new Ambiguous_Permutations();
+    }
+}
--- Small Factorial/Small_Factorial.java
+++ Small Factorial/Small_Factorial.java
@@ -2,6 +2,23 @@ import java.util.*;
 
 class Small_Factorial {
 
+    Scanner sc = new Scanner(System.in);
+    int test = sc.nextInt();// taking the input for test cases
+
+    Small_Factorial() { // our constructor
+        while (test > 0) {
+            int n = sc.nextInt(); // Input for the number
+            int fact = 1;
+            int i = 1;
+            while (i <= n) { // calculating the factorial
+                fact *= i;
+                i++;
+            }
+            System.out.println(fact); // displaying the result
+            test--;
+        }
+    }
+
     public static void main(String args[]) {
         Small_Factorial SF = new Small_Factorial();
 
