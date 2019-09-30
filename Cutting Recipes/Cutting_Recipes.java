e99b883ccfbef18bc6c05b0640273ada5a1655d1
Abhinav Utkarsh <abhinav.utkarsh@gmail.com>
2019-09-30 17:17:49 +0530

    Cutting Recipes solved and Mahasena added

--- Cutting Recipes/Cutting_Recipes.java
+++ Cutting Recipes/Cutting_Recipes.java
@@ -1,12 +1,42 @@
 import java.util.*;
 
-class Cutting_Recipies {
+class Cutting_Recipes {
 
-    Cutting_Recipies() {
-        Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
+
+    Cutting_Recipes() {
+
+        int test = sc.nextInt();
+        while (test != 0) {
+            int size = sc.nextInt();
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++) {
+                array[i] = sc.nextInt();
+            }
+            int initial = GCD(array[0], array[1]);
+            for (int i = 0; i < size; i++) {
+                initial = GCD(initial, array[i]);
+            }
+            for (int i = 0; i < size; i++) {
+                System.out.print(array[i] / initial + " ");
+            }
+            System.out.println();
+            test--;
+        }
+    }
+
+    int GCD(int a, int b) {
+        if (a == b) {
+            return a;
+        } else if (a > b) {
+            return GCD(a - b, b);
+        } else {
+            return GCD(a, b - a);
+        }
     }
 
     public static void main(String args[]) {
-        Cutting_Recipies CR = new Cutting_Recipies();
+        Cutting_Recipes CR = new Cutting_Recipes();
+
     }
 }
