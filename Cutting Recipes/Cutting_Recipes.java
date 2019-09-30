5105052ef175480f7e6880d5e18653728580943d
Abhinav Utkarsh <abhinav.utkarsh@gmail.com>
2019-09-30 18:49:17 +0530

    Comments added in Cutting_Recipes

--- Cutting Recipes/Cutting_Recipes.java
+++ Cutting Recipes/Cutting_Recipes.java
@@ -6,32 +6,36 @@ class Cutting_Recipes {
 
     Cutting_Recipes() {
 
-        int test = sc.nextInt();
+        int test = sc.nextInt(); // taking in the test cases
         while (test != 0) {
-            int size = sc.nextInt();
+            int size = sc.nextInt(); // taking in the array size
             int[] array = new int[size];
             for (int i = 0; i < size; i++) {
-                array[i] = sc.nextInt();
+                array[i] = sc.nextInt(); // taking in the array itself
             }
-            int initial = GCD(array[0], array[1]);
+            int initial = GCD(array[0], array[1]); // getting the initial GCD of 1st two elements
             for (int i = 0; i < size; i++) {
-                initial = GCD(initial, array[i]);
+                initial = GCD(initial, array[i]); // iterating over the array with initial GCD to get the common GCD of
+                                                  // all elements
             }
             for (int i = 0; i < size; i++) {
-                System.out.print(array[i] / initial + " ");
+                System.out.print(array[i] / initial + " "); // displaying the minimum quality of each ingredient
+                                                            // required in balanced ratio
             }
             System.out.println();
             test--;
         }
     }
 
-    int GCD(int a, int b) {
-        if (a == b) {
+    int GCD(int a, int b) { // Our GCD recursive function
+        if (a == b) { // base condition, when the numbers get equal then the GCD would also be the
+                      // number itself
             return a;
-        } else if (a > b) {
-            return GCD(a - b, b);
+        } else if (a > b) { // Euclidean method to find GCD, subtract the larger number from the smaller
+                            // until they become equal
+            return GCD(a - b, b); // when a>b
         } else {
-            return GCD(a, b - a);
+            return GCD(a, b - a); // when b>a
         }
     }
 
