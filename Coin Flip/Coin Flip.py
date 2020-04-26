2e9fa0d18ccaa427ec32df44c67859d83361e443
Abhinav Utkarsh <abhinav.utkarsh@gmail.com>
2020-04-27 01:31:00 +0530

    More problems added

--- Coin Flip/Coin Flip.py
+++ Coin Flip/Coin Flip.py
@@ -1,2 +1,15 @@
-#https://www.codechef.com/LRNDSA01/problems/CONFLIP
-
+# #https://www.codechef.com/LRNDSA01/problems/CONFLIP
+for i in range(int(input())):
+    for j in range(int(input())):
+        I,N,Q=map(int,input().split())
+        if N%2==0:
+            print(N//2)
+        else:
+            Counting=[0,0,0]
+            if I==1:
+                Counting[1]=N//2
+                Counting[2]=N//2 + 1
+            else:
+                Counting[2]=N//2
+                Counting[1]=N//2 + 1
+            print(Counting[Q])
